d671b1bb5265adfbdb646bb217affc280c0abb89
Japhet Sebastian <japhet.sebastian01@gmail.com>
2023-01-19 17:46:32 +0300

    fixes native tests

--- src/test/java/com/assets/management/assets/rest/DepartmentResourceTestIT.java
+++ src/test/java/com/assets/management/assets/rest/DepartmentResourceTestIT.java
@@ -1,7 +1,26 @@
 package com.assets.management.assets.rest;
 
+import static io.restassured.RestAssured.given;
+import static javax.ws.rs.core.HttpHeaders.ACCEPT;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.junit.jupiter.api.Test;
+
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
 class DepartmentResourceTestIT extends DepartmentResourceTest {
+	@Test
+	@Override
+	void shouldOrShouldNotFindDepartments() {	
+		given()
+			.header(ACCEPT, APPLICATION_JSON)
+			.when()
+			.get()
+			.then()
+			 	.statusCode(Status.OK.getStatusCode())
+				.contentType(APPLICATION_JSON); 
+	}
 }
