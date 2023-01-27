7f820e5e8ae872fc6cf9d83fcfb71439de098f77
Japhet Sebastian <japhet.sebastian01@gmail.com>
2023-01-27 16:55:30 +0300

    fixes

--- src/main/java/com/assets/management/assets/model/valueobject/PurchasePerSupplier.java
+++ src/main/java/com/assets/management/assets/model/valueobject/PurchasePerSupplier.java
@@ -1,9 +1,6 @@
 package com.assets.management.assets.model.valueobject;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.quarkus.hibernate.orm.panache.common.ProjectedFieldName;
 import io.quarkus.runtime.annotations.RegisterForReflection;
@@ -14,10 +11,12 @@ public class PurchasePerSupplier {
 //	@NotNull
 //	@Size(min = 2, max = 64)
 //	@Pattern(regexp = "^[\\p{L} .'-]+$", message = "should include only letters ' and - special characters")
+	@JsonProperty("isbn_13")
 	public final String supplierName;
 //	
 //	@Min(1)
 //	@NotNull
+	@JsonProperty("isbn_13")
 	public final Long purchaseCount;
 	
 	public PurchasePerSupplier(@ProjectedFieldName("supplier.name") String supplierName, Long purchaseCount) {
